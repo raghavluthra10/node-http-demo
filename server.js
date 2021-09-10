89bae7c3f5591e4ae867e8308c50405006c8baaa
raghavluthra <raghavluthra1@gmail.com>
2021-09-10 18:07:52 +0530

    seperate folder for routes and controllers added

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+// Route files
+const bootcamps = require('./routes/bootcamps');
+
 //load env vars
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
+const logger = (req, res, next) => {
+    console.log(`${req.method} | ${req.protocol}://${req.get('host')}${req.originalUrl}`);
+    next();    
+};
+
+app.use(logger)
+
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
