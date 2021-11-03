cb1b12050bf76fdd78b95c4a35ec0f821c7d7fc5
raghavluthra10 <77231964+raghavluthra10@users.noreply.github.com>
2021-11-03 17:17:22 +0530

    code for get single bootcamp with id written

--- controllers/bootcamp.js
+++ controllers/bootcamp.js
@@ -6,7 +6,7 @@ const Bootcamp = require('../models/Bootcamp');
 // @access    Public
 exports.getBootcamps = async (req, res, next) => {
     try {
-        const bootcamps = await Bootcamp.find()
+        const bootcamps = await Bootcamp.find();
 
         res.status(200).json({
             success: true,
@@ -23,8 +23,22 @@ exports.getBootcamps = async (req, res, next) => {
 // @desc      Get single bootcamp
 // @route     Get /api/v1/bootcamps/:id
 // @access    Public
-exports.getBootcamp = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Show bootcamp ${req.params.id}` });
+exports.getBootcamp = async (req, res, next) => {
+
+    try {
+        const bootcamp = await Bootcamp.findById(req.params.id);
+
+        if(!bootcamp) {
+            return res.status(400).json({ success: false, msg: "Bootcamp doesn't exists!" })
+        }
+
+        res.status(200).json({ success: true, data: bootcamp });
+
+    } catch (err) {
+        res.status(400).json({ success: false });
+    }
+
+    // res.status(200).json({ success: true, msg: `Show bootcamp ${req.params.id}` });
 }
 
 
