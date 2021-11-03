5fd2e535fb41b8a8e267dc7adbc76ee715b00e44
raghavluthra10 <77231964+raghavluthra10@users.noreply.github.com>
2021-11-03 18:47:02 +0530

    written code for update and delete

--- controllers/bootcamp.js
+++ controllers/bootcamp.js
@@ -10,6 +10,7 @@ exports.getBootcamps = async (req, res, next) => {
 
         res.status(200).json({
             success: true,
+            count: bootcamps.length,
             data: bootcamps
         });
 
@@ -66,14 +67,40 @@ exports.createBootcamp = async (req, res, next) => {
 // @desc      Update Bootcamp
 // @route     PUT /api/v1/bootcamps/:id
 // @access    Private
-exports.updateBootcamp = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Update bootcamp ${req.params.id}` });
+exports.updateBootcamp = async (req, res, next) => {
+    try {
+        const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, { 
+            new: true, // this will show the new updated data
+            runValidators: true // this will run mongoose validators on update
+         })
+    
+        if(!bootcamp) {
+            return res.status(400).json({ success: false, msg: "Bootcamp doesn't exists" })
+        };
+    
+        res.status(200).json({ success: true, msg: `Update bootcamp with ${req.params.id} Id`, data: bootcamp });
+        
+    } catch (error) {
+        res.status(400).json({ success: false });
+    }
 }
 
 
 // @desc      Delete Bootcamp
 // @route     DELETE /api/v1/bootcamps/:id
 // @access    Private
-exports.deleteBootcamp = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
+exports.deleteBootcamp = async (req, res, next) => {
+    try {
+        const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+    
+        if(!bootcamp) {
+            return res.status(400).json({ success: false, msg: "Bootcamp doesn't exists" })
+        };
+    
+        res.status(200).json({ success: true, msg: `Delete bootcamp with ${req.params.id} Id`, data: bootcamp });
+
+    } catch(err) {
+        res.status(400).json({ success: false });
+    }
+    // res.status(200).json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
 }
