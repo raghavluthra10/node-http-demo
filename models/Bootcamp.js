5d5fcf9327aacadaa6cd00972dc6cbeccbb2f1f4
raghavluthra10 <77231964+raghavluthra10@users.noreply.github.com>
2021-11-08 17:01:40 +0530

    slugify added and 'name' value of bootcampSchema slugged

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const BootcampSchema = new mongoose.Schema({
     name: {
@@ -99,5 +100,12 @@ const BootcampSchema = new mongoose.Schema({
         default: Date.now
     }
 });
+
+// create bootcamp slug from the name
+BootcampSchema.pre('save', function (next) {
+    // console.log('slugify Ran', this.name);
+    this.slug = slugify(this.name, { lower: true });
+    next();
+})
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
