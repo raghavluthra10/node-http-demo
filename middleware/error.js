8a2b06f0512e037a08be7bfea682a041763c9a64
raghavluthra10 <77231964+raghavluthra10@users.noreply.github.com>
2021-11-08 14:30:38 +0530

    'validation error' error handler  added

--- middleware/error.js
+++ middleware/error.js
@@ -8,6 +8,7 @@ const errorHandler = (err, req, res, next) => {
     // log to console for dev
     console.log(err);
 
+
     // Mongoose bad object id
     if(err.name === 'CastError') {
         const message = `Bootcamp not found with id of ${err.value}`;
@@ -18,6 +19,12 @@ const errorHandler = (err, req, res, next) => {
     //Mongoose duplicate key
     if(err.code === 11000) {
         const message = 'Duplicate field value entered';
+        error = new ErrorResponse(message, 400);
+    }
+
+    //Mongoose validation error
+    if(err.name === 'ValidationError') {
+        const message = Object.values(err.errors).map(val => val.message);
         error = new ErrorResponse(message, 400);
     }
 
