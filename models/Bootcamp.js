9124b985b17e0fc40563ddf8e8e926c8f9b3e8e6
raghavluthra <raghavluthra1@gmail.com>
2022-03-24 11:25:42 +0530

    backup from local to remote

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -100,4 +100,4 @@ const BootcampSchema = new mongoose.Schema({
     }
 });
 
-module.exports = mongooose.model('Bootcamp', BootcampSchema);
+module.exports = mongoose.model('Bootcamp', BootcampSchema);
